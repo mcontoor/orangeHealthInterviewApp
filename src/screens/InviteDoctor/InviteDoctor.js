d41ae6695b1ae6b86ab28d001c7af1ce9a6b88d4
mcontoor <mcontoor@gmail.com>
2020-09-27 00:44:33 +0530

    add contact list to invite doc screen

--- src/screens/InviteDoctor/InviteDoctor.js
+++ src/screens/InviteDoctor/InviteDoctor.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import CtaButton from '../../components/Button/CtaButton';
+import ContactList from '../../components/ContactList/ContactList';
 
 const styles = StyleSheet.create({
-  font: {
+  contactsTextStyle: {
     fontFamily: 'Montserrat-Medium',
+    fontSize: 14,
+  },
+  titleStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginBottom: 13,
+    alignItems: 'center',
+  },
+  inviteTextStyle: {
+    fontFamily: 'Montserrat-Medium',
+    fontSize: 16,
   },
 });
 
 const InviteDoctor = () => {
+  const [selectedContacts, setSelectedContacts] = useState([]);
+
+  const title = (
+    <View style={styles.titleStyle}>
+      <Text style={styles.inviteTextStyle}>Invite Contacts</Text>
+      <Text style={styles.contactsTextStyle}>
+        {selectedContacts.length} contact selected
+      </Text>
+    </View>
+  );
+
+  const onSelectContact = useCallback(
+    (contact) =>
+      setSelectedContacts((prevContactList) => [...prevContactList, contact]),
+    [],
+  );
+
   return (
     <View>
       <CtaButton />
-      <Text style={styles.font}>Invite Doctor</Text>
+      <ContactList title={title} />
     </View>
   );
 };
