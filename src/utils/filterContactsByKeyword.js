a36ce02636eac332adf9c8a23f0ab61181518736
mcontoor <mcontoor@gmail.com>
2020-09-27 20:19:21 +0530

    add requiredCharLengthArg

--- src/utils/filterContactsByKeyword.js
+++ src/utils/filterContactsByKeyword.js
@@ -1,7 +1,11 @@
-export const filterByKeyword = (key, contacts) => {
-  return contacts.filter(
-    (contact) =>
-      contact.name.toLowerCase().includes(key.toLowerCase()) ||
-      contact.info.includes(key),
-  );
+export const filterByKeyword = (key, contacts, requiredCharLength) => {
+  if (key.length >= requiredCharLength) {
+    return contacts.filter(
+      (contact) =>
+        contact.name.toLowerCase().includes(key.toLowerCase()) ||
+        contact.info?.includes(key.toLowerCase()),
+    );
+  } else {
+    return [];
+  }
 };
