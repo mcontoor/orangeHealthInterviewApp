75738acf9c155105f199a1cafd7ec5f5d489e2f3
mcontoor <mcontoor@gmail.com>
2020-09-27 22:56:39 +0530

    header style fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,19 +4,20 @@ import {useNavigation} from '@react-navigation/native';
 
 const styles = StyleSheet.create({
   header: {
-    height: 68,
+    height: 74,
     width: '100%',
     backgroundColor: '#ffffff',
   },
   button: {
-    marginTop: 18,
+    marginTop: 15,
     marginBottom: 21,
-    marginLeft: 24,
+    marginLeft: 20,
     width: 40,
+    height: 40,
   },
   backButton: {
-    height: 40,
-    width: 40,
+    height: 45,
+    width: 45,
   },
 });
 
