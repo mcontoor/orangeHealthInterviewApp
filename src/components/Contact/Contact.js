bb9ef3c1acc79f837b07ca38a35762a3a31f16c9
mcontoor <mcontoor@gmail.com>
2020-09-27 12:42:15 +0530

    fix styles

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 import Button from '../Button/Button';
+import Avatar from '../Avatar/Avatar';
 
 const styles = StyleSheet.create({
   contactStyle: {
@@ -12,34 +13,43 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row',
   },
-  avatar: {
-    width: 40,
-    backgroundColor: 'blue',
-    height: '100%',
-  },
   contactInfo: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'flex-start',
     paddingLeft: 17,
   },
+  name: {
+    fontFamily: 'MontSerrat-Medium',
+    fontSize: 16,
+    color: '#1c1c1c',
+  },
+  number: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 12,
+    color: '#616161',
+  },
 });
 
 const Contact = ({name, thumbnail, number, isSelected, onPress}) => {
-  console.log(name, number, thumbnail);
-
   const unSelectedIcon = (
     <Image source={require('../../icons/unchecked_selection.png')} />
   );
   const selectedIcon = (
     <Image source={require('../../icons/checked_selection.png')} />
   );
+  const placeholder = name
+    .split(' ')
+    .slice(0, 2)
+    .map((x) => x.substr(0, 1))
+    .join('')
+    .toUpperCase();
   return (
     <Button style={styles.contactStyle} onPress={onPress}>
-      <View style={styles.avatar} />
+      <Avatar img={thumbnail} placeholder={placeholder} />
       <View style={styles.contactInfo}>
-        <Text>{name}</Text>
-        <Text>{number}</Text>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.number}>{number}</Text>
       </View>
       {isSelected ? selectedIcon : unSelectedIcon}
     </Button>
